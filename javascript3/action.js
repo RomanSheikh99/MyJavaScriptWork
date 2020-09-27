dc780852fd5652ceccf18d6df716fd16a03f1b63
Roman Sheikh <71408438+RomanSheikh99@users.noreply.github.com>
2020-09-28 00:53:47 +0600

    Add files via upload

---
+++ javascript3/action.js
@@ -0,0 +1,133 @@
+document.getElementById("onclickbtn1").addEventListener("click",function(){
+	
+	document.getElementById("myimg").src="img/roman.jpg";
+	
+});
+
+document.getElementById("onclickbtn2").addEventListener("click",function(){
+	
+	document.getElementById("myimg").src="img/rubel.jpg";
+	
+});
+
+document.getElementById("onclickbtn3").addEventListener("click",function(){
+	
+	document.getElementById("myimg").src="img/amin.jpg";
+	
+});
+
+
+document.getElementById("onclickbtn4").addEventListener("click",function(){
+	
+	document.getElementById("myimg").src="img/saruar.jpg";
+	
+});
+
+
+document.getElementById("onclickbtn5").addEventListener("click",function(){
+	
+	document.getElementById("myimg").src="img/duha.jpg";
+	
+});
+
+document.getElementById("onclickbtn6").addEventListener("click",function(){
+	
+	document.getElementById("myimg").src="img/milon.jpg";
+	
+});
+
+
+document.getElementById("onclickbtn7").addEventListener("click",function(){
+	
+	document.getElementById("myimg").src="img/samiur.jpg";
+	
+});
+document.getElementById("onclickbtn8").addEventListener("click",function(){
+	
+	document.getElementById("myimg").src="img/shamim.jpg";
+	
+});
+document.getElementById("onclickbtn9").addEventListener("click",function(){
+	
+	document.getElementById("myimg").src="img/tuhin.jpg";
+	
+});
+document.getElementById("onclickbtn10").addEventListener("click",function(){
+	
+	document.getElementById("myimg").src="img/shovon.jpg";
+	
+});
+document.getElementById("onclickbtn11").addEventListener("click",function(){
+	
+	document.getElementById("myimg").src="img/nahid.jpg";
+	
+});
+document.getElementById("onclickbtn12").addEventListener("click",function(){
+	
+	document.getElementById("myimg").src="img/emam.jpg";
+	
+});
+document.getElementById("onclickbtn13").addEventListener("click",function(){
+	
+	document.getElementById("myimg").src="img/joy.jpg";
+	
+});
+document.getElementById("onclickbtn14").addEventListener("click",function(){
+	
+	document.getElementById("myimg").src="img/tarif.jpg";
+	
+});
+document.getElementById("onclickbtn15").addEventListener("click",function(){
+	
+	document.getElementById("myimg").src="img/asad.jpg";
+	
+});
+document.getElementById("onclickbtn16").addEventListener("click",function(){
+	
+	document.getElementById("myimg").src="img/jinto.jpg";
+	
+});
+document.getElementById("onclickbtn17").addEventListener("click",function(){
+	
+	document.getElementById("myimg").src="img/shanto.jpg";
+	
+});
+
+document.getElementById("onclickbtn18").addEventListener("click",function(){
+	
+	document.getElementById("myimg").src="img/prapthy.jpg";
+	
+});
+document.getElementById("onclickbtn19").addEventListener("click",function(){
+	
+	document.getElementById("myimg").src="img/mili.jpg";
+	
+});
+document.getElementById("onclickbtn20").addEventListener("click",function(){
+	
+	document.getElementById("myimg").src="img/rimi.jpg";
+	
+});
+document.getElementById("onclickbtn21").addEventListener("click",function(){
+	
+	document.getElementById("myimg").src="img/raihan.jpg";
+	
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
