57cc6087134550dbbb3bdcde0cd396babd425c67
Roman Sheikh <71408438+RomanSheikh99@users.noreply.github.com>
2020-09-28 00:51:48 +0600

    Add files via upload

---
+++ javascript2/action.js
@@ -0,0 +1,15 @@
+
+
+document.getElementById("onclickbtn1").addEventListener("click",function(){
+
+	document.getElementById("myimg").src="pic_bulbon.gif";
+
+});
+
+document.getElementById("onclickbtn2").addEventListener("click",function(){
+
+	document.getElementById("myimg").src="pic_bulboff.gif";
+
+});
+
+
